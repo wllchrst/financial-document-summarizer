0d159b80e9e57fdaea7ea36d69bd3ab02478b5e8
William Christian <wllchrst@outlook.com>
2025-10-18 11:53:43 +0700

    feat: parse table not done

--- pipelines/extract_pipeline.py
+++ pipelines/extract_pipeline.py
@@ -1,10 +1,13 @@
 ﻿import os
 import pdfplumber
 import pandas as pd
+import cv2
+import numpy as np
 
 from typing import *
 from helpers import WordHelper
 from pdfplumber.page import Page
+from pdf2image import convert_from_path
 
 
 def get_title_codes(folder_name: str = 'data/inlineXBRL') -> List[str]:
@@ -34,44 +37,77 @@ def read_pdf(filepath: str):
     with pdfplumber.open(filepath) as pdf:
         found_starting = False
         pages = []
+        page_indices = []
         current_code = ''
 
         for index, page in enumerate(pdf.pages):
             content = page.extract_text()
-            code_in_content = False
+            code_in_content = None
 
             for code in codes:
                 if code in content:
-                    current_code = code
-                    code_in_content = True
+                    code_in_content = code
                     break
 
             if not found_starting and code_in_content:
                 found_starting = True
+                current_code = code_in_content
                 pages.append(page)
+                page_indices.append(index)
+
+
             elif found_starting and not code_in_content:
                 pages.append(page)
+                page_indices.append(index)
+
             elif found_starting and code_in_content:
                 extraction_result = extract_bilingual_lines(
                     pages=pages,
+                    code=current_code,
                     y_threshold=3,
-                    gap_threshold=40
+                    filepath=filepath,
+                    page_indices=page_indices
                 )
 
                 extractions[current_code] = extraction_result
-                pages = []
-                if len(extractions) == 2:
-                    break
+
+                pages = [page]
+                page_indices = [index]
+                current_code = code_in_content
+
+        if pages and current_code not in extractions:
+            extraction_result = extract_bilingual_lines(
+                pages=pages,
+                code=current_code,
+                y_threshold=3,
+                filepath=filepath,
+                page_indices=page_indices
+            )
+            extractions[current_code] = extraction_result
+
+    return extractions
 
 
-def extract_bilingual_lines(pages, y_threshold=3, gap_threshold=15):
+def extract_bilingual_lines(pages: List[Page],
+                            code: str,
+                            filepath: str,
+                            page_indices: List[int],
+                            y_threshold=3,
+                            gap_threshold=4):
     """
     Groups words by line, then splits them into multiple parts
     based on horizontal gaps (e.g. left + right bilingual text).
+    For each cluster, stores both text and rounded height (max - min vertical range).
     """
-    print(len(pages))
-    data = []
+    if '1410000' not in code:
+        return
+    final_data = []
+    max_height = 0
+    min_height = float('inf')
+
     for page_idx, page in enumerate(pages, start=1):
+        data = []
+        rects = page.rects
         words = page.extract_words(x_tolerance=3, y_tolerance=3)
         lines = {}
 
@@ -91,7 +127,8 @@ def extract_bilingual_lines(pages, y_threshold=3, gap_threshold=15):
         for y, items in sorted(lines.items()):
             items.sort(key=lambda x: x["x0"])
             clusters = []
-            current_cluster = [items[0]["text"]]
+
+            current_cluster = [items[0]]
 
             for i in range(1, len(items)):
                 prev_x = items[i - 1]["x1"]
@@ -99,18 +136,94 @@ def extract_bilingual_lines(pages, y_threshold=3, gap_threshold=15):
                 gap = curr_x - prev_x
 
                 if gap > gap_threshold:
-                    clusters.append(" ".join(current_cluster))
-                    current_cluster = [items[i]["text"]]
-                else:
-                    current_cluster.append(items[i]["text"])
+                    top_vals = [w["top"] for w in current_cluster]
+                    bottom_vals = [w["bottom"] for w in current_cluster]
+                    cluster_height = round(max(bottom_vals) - min(top_vals))
+                    max_height = max(max_height, cluster_height)
+                    min_height = min(min_height, cluster_height)
 
-            clusters.append(" ".join(current_cluster))
+                    clusters.append(make_cluster(current_cluster, height=cluster_height))
+                    current_cluster = [items[i]]
+                else:
+                    current_cluster.append(items[i])
+
+            top_vals = [w["top"] for w in current_cluster]
+            bottom_vals = [w["bottom"] for w in current_cluster]
+            cluster_height = round(max(bottom_vals) - min(top_vals))
+            max_height = max(max_height, cluster_height)
+            min_height = min(min_height, cluster_height)
+
+            clusters.append(make_cluster(current_cluster, height=cluster_height))
             data.append(clusters)
 
-    return data
+        final_grouping = group_vertically(data, rects, max_height=max_height, min_height=min_height)
+
+    return 1
 
 
-def clean_extraction_result(data):
-    print('#' * 100)
-    for d in data:
-        print(d)
+def make_cluster(words, height):
+    top = words[0]['top']
+    bottom = words[0]['bottom']
+    x0, x1 = words[0]['x0'], words[0]['x1']
+
+    return {
+        'text': " ".join(w['text'] for w in words),
+        'height': height,
+        'top': top,
+        'bottom': bottom,
+        'x0': x0,
+        'x1': x1,
+    }
+
+
+def group_vertically(clusters,
+                     rects,
+                     max_height: int,
+                     min_height: int):
+    """
+    Group clusters vertically so that every table row consists of 2 lines,
+    separated by horizontal rectangles.
+    """
+
+    rects = [rect for rect in rects if rect['height'] < 2]
+    rects = sorted(rects, key=lambda r: r['y0'])
+    title = ''
+    top_columns = []
+    value_columns = []
+
+    for index, cluster in enumerate(index, clusters):
+        x = 0
+        if len(cluster) == 1 and cluster[0]['height'] == max_height:
+            title += cluster[0]['text']
+        elif cluster[0]['height'] == max_height:
+            for index, col in enumerate(cluster):
+                if len(top_columns) != len(cluster):
+                    top_columns.append(col['text'])
+                else:
+                    top_columns[index] += col['text']
+        elif cluster[0]['height'] == min_height:
+            value_columns = manage_value_columns(clusters, index)
+
+    for index in range(1, len(rects)):
+        prev_line = rects[index - 1]
+        next_line = rects[index]
+
+        if prev_line['y0'] == next_line['y0']:
+            continue
+
+        group_cluster = []
+
+        for cluster in clusters:
+            cluster_top = cluster[0]['top']
+            cluster_bottom = cluster[0]['bottom']
+            if cluster_top > prev_line['y1'] and cluster_bottom < next_line['y0']:
+                group_cluster.append(cluster)
+
+        if len(group_cluster) > 0:
+            lm = 0
+
+def manage_value_columns(clusters, index: int, min_height: int):
+
+
+def clean_extraction_result():
+    pass
