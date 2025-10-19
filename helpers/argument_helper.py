d1ba5e9927bdc8f46c726f75e2d911f067616611
William Christian <wllchrst@outlook.com>
2025-10-19 16:46:24 +0700

    feat: run application using argument parsing

---
+++ helpers/argument_helper.py
@@ -0,0 +1,12 @@
+﻿import argparse
+
+
+class ArgumentHelper:
+    @staticmethod
+    def parse_main_script():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--action", help="Action commands are: 'api', 'extract', 'categorize', 'summarize'")
+        parser.add_argument("--code",
+                            help="Code of account that is going to be use for extraction, categorization and summarization",
+                            default=None)
+        return parser.parse_args()
--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 ﻿from api import run_api
+from pipelines.extraction_pipeline import extract_pdf
+from pipelines.categorization_pipeline import categorize_data
+from helpers.argument_helper import ArgumentHelper
+
+API_ACTION = 'api'
+EXTRACT_ACTION = 'extract'
+CATEGORIZE_ACTION = 'categorize'
+SUMMARIZE_ACTION = 'summarize'
+FINANCIAL_STATEMENT_PATH = "data/FinancialStatement-2024-Tahunan-EKAD.pdf"
 
 
 def main():
-    run_api()
+    args = ArgumentHelper.parse_main_script()
+
+    if args.action == API_ACTION: run_api()
+
+    code = args.code
+    if args.action == EXTRACT_ACTION:
+        extract_pdf(FINANCIAL_STATEMENT_PATH)
+    elif args.action == CATEGORIZE_ACTION:
+        categorize_data(False, filepath=None, code=code)
+    elif args.action == SUMMARIZE_ACTION:
+        pass
 
 
 if __name__ == "__main__":
